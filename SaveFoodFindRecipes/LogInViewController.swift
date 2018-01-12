6da4389784c35f18597d7b9b5e0e83412a00bf4c
Iris Schlundt Bodien <irisschlundtbodien@iriss-macbook-air.local>
2018-01-12 12:17:34 +0100

    frontend

--- SaveFoodFindRecipes/LogInViewController.swift
+++ SaveFoodFindRecipes/LogInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LogInViewController.swift
 //  SaveFoodFindRecipes
 //
 //  Created by Iris Schlundt Bodien on 11-01-18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LogInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
